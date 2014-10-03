9d6d154f608e3ec756d51b2206b6445209d202ac
Alex Castrounis <acastrounis@users.noreply.github.com>
2014-10-03 12:44:23 -0500

    Implemented revealing module and exports

--- datastats.js
+++ datastats.js
@@ -5,5 +5,29 @@
  * Available under MIT license
  */
 (function() {
+  var dStats = {};
+  
+  function sum(arr){
+    var total = 0;
+    
+    for (var i = 0; i < arr.length; i++) {
+      total = total + arr[i];
+    }
+    
+    return total;
+  }
+  
+  function mean(arr){
+    return sum(arr) / arr.length;
+  }
+  
+  dStats.sum = sum;
+  dStats.mean = mean;
+  
+  if (typeof module !== 'undefined') {
+      module.exports = dStats;
+  } else {
+      this.dStats = dStats;
+  }
   
 }.call(this));
