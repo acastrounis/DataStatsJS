0f06313f1faa1302cafa6bba5cf3c5cb7a283914
acastrounis <acdevmail@gmail.com>
2014-10-04 10:44:50 -0500

    Updated build and qa framework

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,11 +1,47 @@
+'use strict';
+
+var paths = {
+		test: ['test'],
+		js: ['./']
+	},
+	files = {
+		grunt: ['./gruntfile.js'],
+		js: ['./**/*.js', './**/*.json'],
+		test: ['./test/*.js']
+	};
+
 module.exports = function(grunt) {
 
   grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json')
+    pkg: grunt.file.readJSON('package.json'),
+    watch: {
+      js: {
+        files: files.js,
+        tasks: ['jshint:all']
+      },
+      test: {
+        files: files.test,
+        tasks: ['']
+      },
+      gruntfile: {
+        files: files.grunt,
+        tasks: ['']
+      }
+    },
+    jshint: {
+      all: {
+        src: paths.js,
+        options: {
+          jshintrc: true,
+          reporter: require('jshint-stylish')
+        }
+      }
+    }
   });
+
+  require('load-grunt-tasks')(grunt);
   
-  grunt.loadNpmTasks('');
-  
-  grunt.registerTask('default', ['']);
+  grunt.registerTask('default', ['watch:js']);
   grunt.registerTask('build', ['']);
+  grunt.registerTask('qa', ['jshint:all']);
 };
